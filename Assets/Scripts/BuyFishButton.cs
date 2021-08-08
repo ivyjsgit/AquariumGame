da9fba7c0cb202cf557835a107b7459c3e5ca8c2
Ivy <ivyjs@hendrix.edu>
2021-08-08 18:57:37 -0500

    Added working purchase button

---
+++ Assets/Scripts/BuyFishButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BuyFishButton : MonoBehaviour
+{
+    // Use this for initialization
+    public GameObject fish;
+    public int cost;
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoneyManager.Instance.GetMoney() >= cost)
+        {
+            button.interactable = true;
+        }
+        else {
+            button.interactable = false;
+        }
+    }
+
+   public void OnClick()
+    {
+        Vector3 PositionToSpawn = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        Instantiate(fish);
+
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
     public TextMeshProUGUI ScoreText;
+
     public string MoneyPrefix = "Money: ";
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
